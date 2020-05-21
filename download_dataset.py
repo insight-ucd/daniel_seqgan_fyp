5e6ac35b34982163442cd316499320355c9ee375
Daniel139 <Daniel139@users.noreply.github.com>
2020-05-21 11:20:50 +0100

    Final Commit

--- download_dataset.py
+++ download_dataset.py
@@ -11,8 +11,7 @@ def exec():
         # print("The exit code was: %d" % list_files.returncode)
 
 def check_files_exist(commands):
-    file = "lastfm-dataset-1K//content/lastfm-dataset-1K/userid-timestamp-artid-artname-traid-traname.tsv"
-
+    file = "lastfm-dataset-1K/userid-timestamp-artid-artname-traid-traname.tsv"
     if os.path.exists(file):
         print("Last.fm file exists")
         return commands[2:]
